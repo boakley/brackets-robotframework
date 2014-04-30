b33b6d2e3bdaaead3dafd4b9d20dcdfa3b16bbc9
Bryan Oakley <bryan.oakley@gmail.com>
2014-04-29 20:50:15 -0500

    detabify

--- hints.js
+++ hints.js
@@ -4,6 +4,8 @@
 define(function(require, exports, module) {
     'use strict';
 
+    var robot = require("./robot");
+
     // 'libraries' should be a list of KeywordLibrary objects
     function HintProvider(libraries) {
         this.libraries = libraries;
@@ -15,7 +17,32 @@ define(function(require, exports, module) {
         this.cell = null; 
     }
 
+    HintProvider.prototype.hasHints = function(editor, implicitChar) {
+        this.editor = editor;
+        if (implicitChar === null) {
+            var cm = editor._codeMirror;
+            var pos = this.editor.getCursorPos();
+            var cell = robot.get_current_cell(cm, pos);
+            this.cell = cell; 
+            this.match = cell.text;
+            this.hints = this._get_hints(cell.text);
+            return (this.hints.length > 0);
+        } // FIXME: add special mode for an implicit char of "["
+        return false;
+    };
+
+    HintProvider.prototype.getHints = function(implicitChar) {
+        return {hints: this.hints, match: this.match, selectInitial: true};
+    };
+
+    HintProvider.prototype.insertHint = function(hint) {
+        this.editor.document.replaceRange(hint, this.cell.start, this.cell.end);
+        return false;
+    };
+
     HintProvider.prototype._get_hints = function(prefix) {
+
+        // get a list of hints
         var pattern = new RegExp("^" + prefix + ".*", "i");
         var hints = []
         // FIXME: I need a "get_local_keywords" method in the
@@ -32,29 +59,6 @@ define(function(require, exports, module) {
         }
         return hints;
     }
-
-    HintProvider.prototype.hasHints = function(editor, implicitChar) {
-        this.editor = editor;
-	if (implicitChar === null) {
-            var cm = editor._codeMirror;
-            var pos = this.editor.getCursorPos();
-            var cell = robot.get_current_cell(cm, pos);
-            this.cell = cell; 
-            this.match = cell.text;
-            this.hints = this._get_hints(cell.text);
-	    return (this.hints.length > 0);
-	} // FIXME: add special mode for an implicit char of "["
-        return false;
-    };
-
-    HintProvider.prototype.getHints = function(implicitChar) {
-        return {hints: this.hints, match: this.match, selectInitial: true};
-    };
-
-    HintProvider.prototype.insertHint = function(hint) {
-        this.editor.document.replaceRange(hint, this.cell.start, this.cell.end);
-        return false;
-    };
 
     exports.HintProvider = HintProvider;
 });
