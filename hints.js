1f3c04c946f0641c0dbf2115823d6a8a13d8e3f6
Bryan Oakley <bryan.oakley@gmail.com>
2014-08-16 17:53:34 -0500

    major rewrite of hints to support implicitChar (but more work remains...)

--- hints.js
+++ hints.js
@@ -22,93 +22,177 @@ define(function(require, exports, module) {
                                "Robot framework hub is unreachable. Robot autocomplete is unavailable. See https://github.com/boakley/robotframework-hub");
     }
 
+    HintProvider.prototype.insertHintOnTab = true;
+
     HintProvider.prototype.hasHints = function(editor, implicitChar) {
 
-	// I really don't like the implementation of this. Classic case
-	// of a quick hack getting out of control.
+        this.editor = editor;
+        this.looking_for = null;
+
         var cm = editor._codeMirror;
         var pos = editor.getCursorPos();
-        var state = cm.getStateAfter(pos.line);
-        var cell = robot.get_current_cell(cm, pos);
-        var hub_url = prefs.get("hub-url");
-        var keyword_url = hub_url + "/api/keywords?pattern=^" + cell.text + "*"
-        var cell_number = robot.get_current_cell_number(cm, pos);
-        var i;
-        var keywords;
-	var meta;
+        var tmp;
+        var state;
+        var cell;
+        var cell_number;
 
-        this.editor = editor;
-        this.hints = [];
-        this.cell = cell; 
+	if (implicitChar === "*") {
+	    // is it a table heading?
+	    tmp = cm.getRange({line: pos.line, ch: 0},pos);
+	    if (tmp.match(/^\*+/)) {
+		return true;
+	    }
+
+	} else if (implicitChar === "v") {
+            // If user types "vv" (but not in the middle of a word),
+            // replace with ${} and treat it as such. This is probably
+            // the wrong place for such shenanigans, but whatever. 
+            tmp = cm.getRange({line: pos.line, ch: pos.ch-3}, 
+                              {line: pos.line, ch: pos.ch-1});
+            if (tmp.match(/\bv$/)) {
+                cm.replaceRange('${}',{line: pos.line, ch: pos.ch-2}, pos);
+                cm.setCursor({line: pos.line, ch: pos.ch});
+                this.looking_for = "variable";
+                return true;
+            }
+
+        } else if (implicitChar === "{") {
+            tmp = cm.getRange({line: pos.line, ch: pos.ch-2},  pos);
+            if (tmp === "${") {
+                return true;
+            }
+        } else if (implicitChar === "[") {
+            // only provide hints if they are in the first cell
+            // of a testcase or keyword
+            var cm = this.editor._codeMirror;
+            var pos = this.editor.getCursorPos();
+            var state = cm.getStateAfter(pos.line);
+            var cell_number = robot.get_current_cell_number(cm, pos);
+	    if ((state.isTestCasesTable() || state.isKeywordsTable()) && 
+                cell_number == 1) {
+                return true;
+            }
+        }
 
         if (implicitChar === null) {
-
-            // FIXME: this should be only the part of the cell 
-            // leading up to the insertion cursor...
-            this.match = cell.text;
-
-            if (cell.text.match(/\[.*?/) && cell_number === 1) {
-		// remove trailing ], in case it was added by the auto-brace-matcher
-		// (or by the user...).
-		this.hints = get_metadata_hints(cell.text, state);
-
-            } else if (cell.text.match(/^\*+\s*/)) {
-		var prefix = cell.text.replace(/^\*+\s*/,'');
-		this.hints = get_heading_hints(prefix);
-
-            } else if (state.isSettingsTable() && cell_number === 0) {
-		this.hints = get_settings_hints(cell.text);
-
-	    } else {
-		// likely a keyword
-
-		// FIXME: move fetching keyword data to a function,
-		// and give the function the ability to add a status
-		// indicator to the statusbar if the hub is down
-		$.ajaxSetup({ "async": false});
-		keywords = []
-		// We should be able to catch errors via .fail, but
-		// we need to work around a bug in jquery.
-		// See http://bugs.jquery.com/ticket/14683
-		try {
-                    var response = $.getJSON(keyword_url, function(data) {
-			StatusBar.updateIndicator("rfhub-error", false)
-			for (i = 0; i < data.keywords.length; i++) {
-			    keywords.push(data.keywords[i].name + "<i>&nbsp;(" + data.keywords[i].library + ")</i>")
-					  
-//                            keywords.push({library: data.keywords[i].library,
-//					   keyword: data.keywords[i].name})
-					 
-			}
-                    }).fail(function(jqxhr, textStatus, error) {
-			StatusBar.updateIndicator("rfhub-error", true)
-                    })
-		} 
-		catch(err) {
-                    StatusBar.updateIndicator("rfhub-error", true)
-		}
-
-		$.ajaxSetup({ "async": true});
-		this.hints = keywords.sort();
-	    }
-            return this.hints.length > 0;
+            return true;
         }
         return false;
-    };
+    }
 
     HintProvider.prototype.getHints = function(implicitChar) {
-        return {hints: this.hints, match: this.match, selectInitial: true};
+        var cm = this.editor._codeMirror;
+        var pos = this.editor.getCursorPos();
+        var state = cm.getStateAfter(pos.line);
+        var cell = robot.get_current_cell(cm, pos);
+        var cell_number = robot.get_current_cell_number(cm, pos);
+        var match = "";
+        var hints;
+
+        this.cell = cell;
+
+	if (cell.text.match(/^\s*\[/)) {
+	    // metadata
+            match = cell.text.replace(/^\s*\[|\]\s*$/g, "");
+            hints = get_metadata_hints(match, state);
+            return {hints: hints, match: match, selectInitial: true};
+
+        } else if (cell.text.match(/\s*\$\{/)) {
+	    // variables
+            // FIXME: either remove the current cell from the list,
+            // or make it the first item in the list
+            var matches = get_valid_variables(this.editor, pos);
+            matches = matches.filter(function(item, index, inputArray) {
+                // filter the list by only using matches that contain
+                // the current cell text (minus the ${ and })
+                var itemname = item.replace(/^\${|}$/g, "");
+                var match = cell.text.replace(/^\${|}$/g, "");
+                var result = (itemname.toLowerCase().indexOf(match.toLowerCase()) == 0);
+                return result;
+            });
+            var hints = make_ordered_set(matches);
+            return {hints: hints, match: match, selectInitial: true};
+
+        } else if (cell.text.match(/\*+/) && cell.start.ch === 0) {
+	    // table headings
+ 	    match = cell.text.replace(/^\*+\s*|\*\s*/,'');
+
+	    hints = get_heading_hints(match)
+	    return {hints: hints, match: match, selectInitial: true};
+	} else if (state.isSettingsTable() && cell_number === 0) {
+	    // settings
+	    hints = get_settings_hints(cell.text);
+	    return {hints: hints, match: cell.text, selectInitial: true};
+	} else {
+
+	    // ok, done with all the special cases. What's left might be a keyword
+	    // FIXME: move fetching keyword data to a function,
+	    // and give the function the ability to add a status
+	    // indicator to the statusbar if the hub is down
+
+            var hub_url = prefs.get("hub-url");
+            var keyword_url = hub_url + "/api/keywords?pattern=^" + cell.text + "*"
+		var keywords = []
+
+	    $.ajaxSetup({ "async": false});
+	    // We should be able to catch errors via .fail, but
+	    // we need to work around a bug in jquery.
+	    // See http://bugs.jquery.com/ticket/14683
+	    try {
+		var response = $.getJSON(keyword_url, function(data) {
+		    StatusBar.updateIndicator("rfhub-error", false)
+		    for (i = 0; i < data.keywords.length; i++) {
+			keywords.push(data.keywords[i].name + "<i>&nbsp;(" + data.keywords[i].library + ")</i>")
+			
+			//                            keywords.push({library: data.keywords[i].library,
+			//					   keyword: data.keywords[i].name})
+			
+		    }
+		}).fail(function(jqxhr, textStatus, error) {
+		    StatusBar.updateIndicator("rfhub-error", true)
+		})
+	    } 
+	    catch(err) {
+		StatusBar.updateIndicator("rfhub-error", true)
+	    }
+
+	    $.ajaxSetup({ "async": true});
+	    return {hints: keywords.sort(), match: cell.text, selectInitial: true};
+	}
     };
 
     HintProvider.prototype.insertHint = function(hint) {
         // The hint may contain the library name surrounded
         // by <i></i>, which needs to be stripped off
         hint = hint.replace(/<i>.*<\/i>/,"");
+
+        // this next piece of magic should probably be configurable
+        // by the user - let's auto-add a separator. I think it's
+        // the Right Thing to do more often than not.
+	if (hint.match(/^\*/)) {
+	    // a heading
+	    hint += "\n";
+	} else {
+            hint += " | ";
+	}
+        
         this.editor.document.replaceRange(hint, this.cell.start, this.cell.end);
+
         return false;
     };
 
+    // this function is supposed to return a list of variables 
+    // visible at the current position. What it actually does
+    // is return all variables mentioned in the file. Not
+    // ideal, but it's a good starting place.
+    function get_valid_variables(editor, pos) {
+        var matches = editor.document.getText().match(/\${.*?}/g);
+        return matches;
+    }
+    
     function get_settings_hints(prefix) {
+	// I hate hard-coding these. I wish robot had an API I could use
+	// to fetch them. 
         var allowable = ["Library","Resource","Variables",
             "Documentation", "Metadata", 
             "Suite Setup", "Suite Teardown",
@@ -132,8 +216,6 @@ define(function(require, exports, module) {
     }
 
     function get_metadata_hints(prefix, state) {
-	// possible metadata; prefix is expected to have a leading
-	// square bracket; a trailing square bracket will be ignored.
 	var meta = [];
 	var hints = [];
 	var i;
@@ -145,7 +227,7 @@ define(function(require, exports, module) {
 		    "[Teardown]", "[Timeout]"];
 	}
 
-	prefix = prefix.replace(/\]$/g, "").toLowerCase();
+	prefix = "[" + prefix.toLowerCase();
 	for (i = 0; i < meta.length; i++) {
 	    if (meta[i].toLowerCase().indexOf(prefix) == 0) {
 		hints.push(meta[i])
@@ -174,6 +256,21 @@ define(function(require, exports, module) {
         }
     };
 
+    function make_ordered_set(array) {
+        // remove duplicates, and do case-insensitive sort
+        array = array.filter(function(item, index, inputArray) {
+            return inputArray.indexOf(item) == index;
+        });
+
+        array = array.sort(function (a,b) {
+            if (a.toLowerCase() < b.toLowerCase()) return -1;
+            if (a.toLowerCase() > b.toLowerCase()) return 1;
+            return 0;
+        });
+
+        return array
+    }
+    
     HintProvider.prototype._get_hints = function(state, prefix) {
 
         var escaped_prefix = prefix.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") ;
@@ -196,7 +293,6 @@ define(function(require, exports, module) {
                      "*** Keywords ***",
                      "*** Variables ***"]
         } else {
-	    console.log("***** WTF?", this.libraries)
             for (var i=0; i < this.libraries.length; i++) {
                 var lib = this.libraries[i];
                 var tmp = lib.keywords(pattern);
