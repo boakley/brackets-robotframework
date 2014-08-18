01bc582dc2790cf9b2521909051ffa63166c256f
Bryan Oakley <bryanoakley@echo.com>
2014-08-18 16:59:46 -0500

    clean up some jslint errors

--- inlinedocs.js
+++ inlinedocs.js
@@ -1,4 +1,7 @@
-define(function(require, exports, module) {
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true, */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
     'use strict';
 
     var PreferencesManager = brackets.getModule("preferences/PreferencesManager");
@@ -8,25 +11,35 @@ define(function(require, exports, module) {
     var robot = require("./robot");
     var InlineDocsViewer = require("InlineDocsViewer");
 
-    function callRFHub(url, result) {
+    function callRFHub(url) {
         $.ajaxSetup({ "async": false});
         var result = null;
-        var response = $.getJSON(url, function(data) {
-            StatusBar.updateIndicator("rfhub-error", false)
+        var response = $.getJSON(url, function (data) {
+            StatusBar.updateIndicator("rfhub-error", false);
             result = data;
 
-        }).fail(function(jqxhr, textStatus, error) {
-            StatusBar.updateIndicator("rfhub-error", true)
-        })
+        }).fail(function (jqxhr, textStatus, error) {
+            StatusBar.updateIndicator("rfhub-error", true);
+        });
         $.ajaxSetup({ "async": true});
-        return result
+        return result;
+    }
+    
+    function getKeyword(editor, pos) {
+        var cm = editor._codeMirror;
+        var cell = robot.get_current_cell(cm, pos);
+        if (cell.text.trim().substring(0, 1) === "#") {
+            // this is a comment, so ignore it
+            return null;
+        }
+        return cell.text;
     }
 
     function getKeywordDocs(editor, pos) {
         var keyword = getKeyword(editor, pos);
         var prefs = PreferencesManager.getExtensionPrefs("robotframework");
         var hub_url = prefs.get("hub-url");
-        var url = hub_url + "/api/keywords?pattern=^" + keyword + "";
+        var url = hub_url + "/api/keywords?pattern=^" + keyword;
 
         var result = null;
         var data = callRFHub(url);
@@ -38,45 +51,45 @@ define(function(require, exports, module) {
                 library:     data.keywords[0].library,
                 library_url: data.keywords[0].library_url,
                 name:        data.keywords[0].name
-            }
+            };
             return result;
         }
         return null;
     }
 
-    function getKeyword(editor, pos) {
-        var cm = editor._codeMirror;
-        var cell = robot.get_current_cell(cm, pos);
-        if (cell.text.trim().substring(0,1) === "#") {
-            // this is a comment, so ignore it
-            return null;
-        }
-        return cell.text;
-    }
 
     function inlineDocsProvider(editor, pos) {
 
+        var langId,
+            sel,
+            kw,
+            docs,
+            tmp,
+            inlineWidget,
+            result;
+
         // Only provide docs when cursor is in php ("clike") content
-        var langId = editor.getLanguageForSelection().getId();
+        langId = editor.getLanguageForSelection().getId();
         if (langId !== "robot") {
             return null;
         }
 
         // Only provide docs if there is no multiline selection
-        var sel = editor.getSelection();
+        sel = editor.getSelection();
         if (sel.start.line !== sel.end.line) {
             return null;
         }
 
         // Get the keyword near the cursor
-        var kw = getKeyword(editor, pos)
+        kw = getKeyword(editor, pos);
 
-        var docs = getKeywordDocs(editor, pos);
+        // Get the docs for the keyword
+        docs = getKeywordDocs(editor, pos);
         if (docs === null) {
             return false;
         }
-
-        var inlineWidget = new InlineDocsViewer(docs.name, {
+        
+        inlineWidget = new InlineDocsViewer(docs.name, {
             keyword_name: docs.name,
             keyword_doc: docs.htmldoc,
             keyword_args: docs.args,
@@ -84,7 +97,7 @@ define(function(require, exports, module) {
             rfhub_url: "http://localhost:7070/doc/keywords/BuiltIn/Repeat%20Keyword"
         });
 
-        var result = new $.Deferred();
+        result = new $.Deferred();
         inlineWidget.load(editor);
         result.resolve(inlineWidget);
         return result.promise();
